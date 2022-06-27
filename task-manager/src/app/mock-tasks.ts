220bec86a39818a56b128688566d21912825376b
Yan Cancella <86201622+Yancanc@users.noreply.github.com>
2022-06-27 02:51:37 -0300

    Display das tasks dentro da web

---
+++ task-manager/src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Entregar o App',
+    day: '27 de junho de 2022 ás 15:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Ir para a faculdade',
+    day: '27 de junho de 2022 ás 18:45',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Dormir',
+    day: '27 de junho de 2022 ás 23:00',
+    reminder: false,
+  },
+];
